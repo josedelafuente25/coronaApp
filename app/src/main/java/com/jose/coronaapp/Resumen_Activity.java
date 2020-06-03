fce101e960e9814ddf7e04ab314038f77e40a6fb
josedelafuente25 <jose.dlf25@gmail.com>
2020-06-03 13:03:59 -0400

    se agrega captura de fecha automatica

--- app/src/main/java/com/jose/coronaapp/Resumen_Activity.java
+++ app/src/main/java/com/jose/coronaapp/Resumen_Activity.java
@@ -4,18 +4,29 @@ package com.jose.coronaapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Resumen_Activity extends AppCompatActivity {
+    TextView fechaCompleta;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pantalla_resumen);
         Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
 
+        Date d=new Date();
+
+        fechaCompleta = (TextView) findViewById(R.id.txt_fecha);
+        SimpleDateFormat fecc=new SimpleDateFormat("d, MMMM 'del'yyyy");
+        String fechacComplString = fecc.format(d);
+        fechaCompleta.setText(fechacComplString);
 
     }
 
@@ -23,6 +34,9 @@ public class Resumen_Activity extends AppCompatActivity {
         Intent pantalla_inicio  = new Intent(this,MainActivity.class);
     startActivity(pantalla_inicio);
         Toast.makeText(this, "Pantalla Inicio", Toast.LENGTH_SHORT).show();
+
+
 }
+
 }
 
