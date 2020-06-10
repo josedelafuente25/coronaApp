260ac1d168a4832822de8626b90aa83cb0dc36b2
josedelafuente25 <jose.dlf25@gmail.com>
2020-06-09 23:38:52 -0400

    Se incorporan Spinner para seleccionar región en las pantallas de resumen, gráfico y mapa

--- app/src/main/java/com/jose/coronaapp/MapaActivity.java
+++ app/src/main/java/com/jose/coronaapp/MapaActivity.java
@@ -5,15 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MapaActivity extends AppCompatActivity {
+    Spinner comboRegiones;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
 
+        comboRegiones= (Spinner) findViewById(R.id.spinnerRegiones);
+
+        final ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(
+                this, R.array.combo_regiones, android.R.layout.simple_spinner_item);
+
+
+        comboRegiones.setAdapter(adapter);
+
+        comboRegiones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
     }
 
--- app/src/main/java/com/jose/coronaapp/Resumen_Activity.java
+++ app/src/main/java/com/jose/coronaapp/Resumen_Activity.java
@@ -4,24 +4,28 @@ package com.jose.coronaapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.ArrayLinkedVariables;
+
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Set;
 
 public class Resumen_Activity extends AppCompatActivity {
-
    private TextView fechaCompleta;
-
-   // private Button btn_atras;
-   // private Button btn_grafico;
-    //private Button btn_mapa;
+   private TextView cambiarNombre;
+   Spinner comboRegiones;
 
 
-
-
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pantalla_resumen);
 
@@ -32,19 +36,30 @@ public class Resumen_Activity extends AppCompatActivity {
         String fechacComplString = fecc.format(d);
         fechaCompleta.setText(fechacComplString);
 
-       // btn_grafico =findViewById(R.id.btn_grafico);
-      //  btn_mapa =findViewById(R.id.btn_mapa);
-       // btn_atras =findViewById(R.id.btn_atras);
+
+        comboRegiones= (Spinner) findViewById(R.id.spinnerRegiones);
+       // cambiarNombre= findViewById(R.id.nombreTextV);
+
+
+
+        final ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(
+                this, R.array.combo_regiones, android.R.layout.simple_spinner_item);
+
+
+        comboRegiones.setAdapter(adapter);
+
+        comboRegiones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //cambiarNombre.setText(("A"));
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
     }
 
-
-   // public void volver(View view) {
-       // Intent pantalla_inicio = new Intent(this, MainActivity.class);
-       // startActivity(pantalla_inicio);
-        //Toast.makeText(this, "Pantalla Inicio", Toast.LENGTH_SHORT).show();
-
-
-   // }
 
     public void cambiar_pantalla(View view) {
         int opcion = view.getId();
